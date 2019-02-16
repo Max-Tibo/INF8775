7a680333077d32cea07ae412124ef5f9d5701e51
MaxThi <maxime.thibault.371@outlook.com>
2019-02-16 14:33:39 -0500

    ajout code pour construire tableau de résulktats

--- tp1-H19/exemple/src/main.py
+++ tp1-H19/exemple/src/main.py
@@ -18,10 +18,10 @@ def read_integers(filename):
 # create the power test from a results csv file
 # based on the moodle "guide bash"
 def power_test(csvfile, serie):
-    df = pd.read_csv(csvfile).groupby(['Sample_Sizes','Algorithms']).mean().reset_index()
+    df = pd.read_csv(csvfile).groupby(['Sample Size','Algorithm']).mean().reset_index()
 
-    g = sns.FacetGrid(df, hue='Algorithms', height=6, aspect=1)
-    g = g.map(plt.plot, 'Sample_Sizes', 'Times')
+    g = sns.FacetGrid(df, hue='Algorithm', height=6, aspect=1)
+    g = g.map(plt.plot, 'Sample Size', 'Time')
     g.set(xscale='log')
     g.set(yscale='log')
     g.add_legend()
@@ -51,55 +51,76 @@ times3 = [] # quickRandomSeuil
 times4 = [] # coutning
 
 # open csv file to write results
-with open('results' + input + '.csv', 'w', newline='') as myfile:
-    columnTitles = ["Sample_Sizes", "Algorithms", "Times"]
+with open('graphresults' + input + '.csv', 'w', newline='') as myfile:
+    columnTitles = ["Sample Size", "Algorithm", "Time"]
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     wr.writerow(columnTitles)
 
-for size in sizes:
-    sortTime1 = 0.0
-    sortTime2 = 0.0
-    sortTime3 = 0.0
-    sortTime4 = 0.0
-    countingSortErr = 0
-    for test in range(testset[0], testset[1]):
-        file = "../../exemplaires/testset_" + str(size) + "_" + str(test) + ".txt"
-        array = []
-        tempArray = read_integers(file)
-        # execute the algorithms 
-        # quickSort
-        array = deepcopy(tempArray)
-        sortTime1 += quickSort(array)
+with open('results' + input + '.csv', 'w', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    columnTitles = ["Algorithm"]
+    quickResults = ["QuickSort"]
+    quickSeuilResults = ["QuickSortSeuil"]
+    quickRandomSeuilResults = ["QuickSortRandomSeuil"]
+    countingResults = ["CountingSort"]
 
-        # quickSort with seuil
-        array = deepcopy(tempArray)
-        sortTime2 += quickSortSeuil(array)
-
-        # quickSort with seuil and random pivot
-        for i in range(0, 10):
-            array = deepcopy(tempArray)
-            sortTime3 += quickSortRandomSeuil(array)
-        sortTime3 += (sortTime3/10)
-
-        # countingSort
+    for size in sizes:
+        sortTime1 = 0.0
+        sortTime2 = 0.0
+        sortTime3 = 0.0
+        sortTime4 = 0.0
         tmpTime = 0.0
-        array = deepcopy(tempArray)
-        tmpTime = countingSort(array)
-        if tmpTime > 0:
-            sortTime4 += tmpTime
-        else:
-            countingSortErr += 1
-    
-    sortTime1 = round(1000 * sortTime1/10, 3)
-    times1.append(sortTime1)
-    sortTime2 = round(1000 * sortTime2/10, 3)
-    times2.append(sortTime2)
-    sortTime3 = round(1000 * sortTime3/10, 3)
-    times3.append(sortTime3)
-    sortTime4 = round(1000 * sortTime4/(10 - countingSortErr), 3)
-    times4.append(sortTime4)
+        countingSortErr = 0
+        for test in range(testset[0], testset[1] + 1):
+            columnTitles.append("testset_" + str(size) + "_" + str(test))
+            file = "../../exemplaires/testset_" + str(size) + "_" + str(test) + ".txt"
+            array = []
+            tempArray = read_integers(file)
+            # execute the algorithms 
+            # quickSort
+            array = deepcopy(tempArray)
+            tmpTime = quickSort(array) * 1000.0
+            quickResults.append(tmpTime)
+            sortTime1 += tmpTime
 
-with open('results' + input + '.csv', 'a', newline='') as myfile:
+            # quickSort with seuil
+            array = deepcopy(tempArray)
+            tmpTime = quickSortSeuil(array) * 1000.0
+            quickSeuilResults.append(tmpTime)
+            sortTime2 += tmpTime
+
+            # quickSort with seuil and random pivot
+            for i in range(0, 10):
+                array = deepcopy(tempArray)
+                tmpTime += quickSortRandomSeuil(array) * 1000.0
+            quickRandomSeuilResults.append(tmpTime/10.0)
+            sortTime3 += (tmpTime/10.0)
+
+            # countingSort
+            array = deepcopy(tempArray)
+            tmpTime = countingSort(array) * 1000.0
+            countingResults.append(tmpTime)
+            if tmpTime > 0:
+                sortTime4 += tmpTime
+            else:
+                countingSortErr += 1
+
+        sortTime1 = round(sortTime1/10, 3)
+        times1.append(sortTime1)
+        sortTime2 = round(sortTime2/10, 3)
+        times2.append(sortTime2)
+        sortTime3 = round(sortTime3/10, 3)
+        times3.append(sortTime3)
+        sortTime4 = round(sortTime4/(10 - countingSortErr), 3)
+        times4.append(sortTime4)
+
+    wr.writerow(columnTitles)
+    wr.writerow(quickResults)
+    wr.writerow(quickSeuilResults)
+    wr.writerow(quickRandomSeuilResults)
+    wr.writerow(countingResults)
+
+with open('graphresults' + input + '.csv', 'a', newline='') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     for i in range(0, len(sizes)):
         resultRow = []
@@ -124,4 +145,4 @@ with open('results' + input + '.csv', 'a', newline='') as myfile:
         wr.writerow(resultRow)
         resultRow.clear()        
 
-power_test('results' + input + '.csv', input)
+power_test('graphresults' + input + '.csv', input)
